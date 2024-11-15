fde20dd430e02d9b6133a49c63272c0632811c63
Stephen Staklinski <109625525+StephenStaklinski@users.noreply.github.com>
2024-11-15 09:50:49 -0500

    Update metastabayes name to beam

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-# Metastabayes BEAST2 package for Bayesian cancer metastasis graph inference from CRISPR cell lineage tracing data
+# Bayesian Evolutionary Analysis of Metastasis (BEAM) BEAST2 package for Bayesian cancer migration graph inference from CRISPR cell lineage tracing data
 
 ## Inference
 
-Install java+fx 17.0.9 and BEAGLE and set the path to it in `BEAGLE_LIB_PATH`. Then, the provided `metastabayes.jar` file is all that is needed to run the method with:
+Install java+fx 17.0.9 and BEAGLE and set the path to it in `BEAGLE_LIB_PATH`. Then, the provided `beam.jar` file is all that is needed to run the method with:
 
 ```
 # specify path to the repo
-REPO_DIR="/Users/staklins/projects/crispr-barcode-cancer-metastasis/metastabayes_dev/metastabayes"
+REPO_DIR="/Users/staklins/projects/crispr-barcode-cancer-metastasis/beam_dev/beam"
 
 # submit example data to run
-java -Djava.library.path=$BEAGLE_LIB_PATH -jar metastabayes.jar \
+java -Djava.library.path=$BEAGLE_LIB_PATH -jar beam.jar \
 -seed 1724005224593 \
 -D repo=$REPO_DIR \
 -overwrite \
@@ -21,8 +21,8 @@ java -Djava.library.path=$BEAGLE_LIB_PATH -jar metastabayes.jar \
 An initial install will obtain dependencies for developing on the code base by running the following with the path to Java FX specified:
 ```
 # make the working directory
-mkdir metastabayes_dev
-cd metastabayes_dev
+mkdir beam_dev
+cd beam_dev
 
 # handle feast external to ant
 # specify path to Java FX (or OpenJDK) and compile feast
@@ -30,12 +30,12 @@ gh repo clone tgvaughan/feast
 cd feast
 JAVA_FX_HOME="/Users/staklins/bin/zulu17.46.19-ca-fx-jdk17.0.9-macosx_aarch64/lib" ant
 
-# compile metastabayes code and related dependencies
-cd ../metastabayes
+# compile BEAM code and related dependencies
+cd ../beam
 ant
 ```
 
-Then, any further changes in the code base can be reflected by running `ant` in the `metastabayes_dev/metastabayes` directory.
+Then, any further changes in the code base can be reflected by running `ant` in the `beam_dev/beam` directory.
 
 
 
---
+++ src/beam/substitutionmodel/BeamGeneralTissueSubstitutionModel.java
@@ -0,0 +1,92 @@
+package beam.substitutionmodel;
+
+import java.util.HashMap; 
+
+import beast.base.core.Input;
+import beast.base.core.Input.Validate;
+import beast.base.core.Description;
+import beast.base.core.Function;
+import beast.base.inference.parameter.IntegerParameter;
+import beast.base.inference.parameter.BooleanParameter;
+import beast.base.inference.parameter.Parameter;
+import beast.base.evolution.substitutionmodel.ComplexSubstitutionModel;
+
+/**
+ * @author Stephen Staklinski
+ **/
+
+@Description("Substitution model with structure indicators to setup shared rate paramaters between tissues")
+
+public class BeamGeneralTissueSubstitutionModel extends ComplexSubstitutionModel {
+	
+    public Input<IntegerParameter> structure = new Input<IntegerParameter>("matrixStructure",
+            "integer indices to indicate structure of shared transition rate matrix parameters between diferent tissues", Validate.OPTIONAL);
+
+    private IntegerParameter matrixStructure;
+
+	@Override
+    public void initAndValidate(){
+
+        super.initAndValidate();
+
+        matrixStructure = structure.get();
+        nrOfStates = (int) Math.sqrt(matrixStructure.getDimension());
+        rateMatrix = new double[nrOfStates][nrOfStates];
+
+        int numRelativeRates = relativeRates.length;
+
+        HashMap<Integer,Integer> uniqueMatrixStructure = new HashMap<Integer,Integer>();
+        for (int j = 0; j < matrixStructure.getDimension(); j++) {   
+            uniqueMatrixStructure.put(matrixStructure.getValue(j), j);   
+        }
+        int countUniqueMatrixStructure = uniqueMatrixStructure.keySet().size();
+
+        if (numRelativeRates != countUniqueMatrixStructure) {
+            throw new IllegalArgumentException("Number of relative rates must match the number of unique rates specified in the matrix structure.");
+        }
+    }
+
+    /** sets up rate matrix **/
+    @Override
+    public void setupRateMatrix() {
+
+        // new code to allow specification of the rate matrix structure for parameter sharing
+        int n = 0;
+        for (int i = 0; i < nrOfStates; i++) {
+            for (int j = 0; j < nrOfStates; j++) {
+                rateMatrix[i][j] = relativeRates[matrixStructure.getValue(n)];
+                n++;
+            }
+        }
+
+        // bring in frequencies
+        double [] freqs = frequencies.getFreqs();
+        for (int i = 0; i < nrOfStates; i++) {
+            for (int j = i + 1; j < nrOfStates; j++) {
+                rateMatrix[i][j] *= freqs[j];
+                rateMatrix[j][i] *= freqs[i];
+            }
+        }
+        
+        // set up diagonal
+        for (int i = 0; i < nrOfStates; i++) {
+            double sum = 0.0;
+            for (int j = 0; j < nrOfStates; j++) {
+                if (i != j)
+                    sum += rateMatrix[i][j];
+            }
+            rateMatrix[i][i] = -sum;
+        }
+
+        // normalise rate matrix to one expected substitution per unit time
+        double subst = 0.0;
+        for (int i = 0; i < nrOfStates; i++)
+            subst += -rateMatrix[i][i] * freqs[i];
+
+        for (int i = 0; i < nrOfStates; i++) {
+            for (int j = 0; j < nrOfStates; j++) {
+                rateMatrix[i][j] = rateMatrix[i][j] / subst;
+            }
+        }
+    }
+}
