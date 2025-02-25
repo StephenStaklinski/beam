c03e58e2942e939be0700a3f2a0957318ab85a52
Stephen Staklinski <109625525+StephenStaklinski@users.noreply.github.com>
2025-02-25 14:58:45 -0500

    Update examples

---
+++ examples/quinn_cp26/submit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+java -Djava.library.path=$BEAGLE_LIB_PATH -Xmx10g -jar /Users/staklins/projects/crispr-barcode-cancer-metastasis/beam_dev/beam/beam.jar \
+-seed 12345 \
+-threads 5 \
+-overwrite \
+-working \
+/Users/staklins/projects/crispr-barcode-cancer-metastasis/beam_dev/beam/examples/quinn_cp26/1.xml
+
+
