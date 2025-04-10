7578ff26da1a14e73adcf4e7ef4d6b8aab3a3f1d
StephenStaklinski <stephenstak@gmail.com>
2025-04-10 17:10:18 -0400

    Update examples to use birth death model without bdsky

--- examples/quinn_cp26/submit.sh
+++ examples/quinn_cp26/submit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-java -Djava.library.path=$BEAGLE_LIB_PATH -Xmx10g -jar /Users/staklins/projects/crispr-barcode-cancer-metastasis/beam/beam.jar \
+java -Djava.library.path=$BEAGLE_LIB_PATH -Xmx10g -jar /grid/siepel/home/staklins/beam/beam.jar \
 -seed 12345 \
 -threads 5 \
 -overwrite \
 -working \
-/Users/staklins/projects/crispr-barcode-cancer-metastasis/beam/examples/quinn_cp26/1.xml
+/grid/siepel/home/staklins/beam/examples/quinn_cp26/1.xml
 
 
